ac57ce61f2ca1ef20ff9adb2cada3292f0bf7dc0
SUPHAKKHAWII6812022610298 <s6812022610298@email.kmutnb.ac.th>
2025-08-27 16:43:58 +0700

    WORK compro_w07 WAVE

---
+++ Coding_w07-01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float score = 60;                         // กำหนดคะแนนสอบ midterm
+    float final_score;
+
+    if (score >= 50) {                     // คะแนน มากกว่าหรือเท่ากับ 50
+        final_score = score * 5%;       // คิด 5% ของคะแนนที่ได้จริง
+    } else {                             //คำสั่ง else จะแสดงผลต่อเมื่อไม่สามารถ เข้าเงื่อนไข if
+        final_score = score;            // คะแนนเท่ากับคะแนนที่กำหนด
+    }
+
+    printf("Final score: %.2f\n", final_score);
+    printf("End of evaluation\n");
+
+    return 0;
+}
+/*คำสั่ง else คือถ้าหากคะแนนน้อยกว่า ที่ if กำหนด จะแสดงค่า ที่ได้หนด เช่น if หนดที่มากกว่าหรือเท่ากับ 50 แต่คะแนนได้ 45 eles จะแสดงค่า 45 */
+//1.05;
